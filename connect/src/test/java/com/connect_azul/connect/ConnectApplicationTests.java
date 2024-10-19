054b48a9b0d9e07307fbc6e843c92886085e450b
Arthur Holanda <holandaarthur46@gmail.com>
2024-10-19 15:44:54 -0300

    Geração do projeto

---
+++ connect/src/test/java/com/connect_azul/connect/ConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connect_azul.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
